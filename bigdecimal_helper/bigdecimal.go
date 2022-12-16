981a9dd801dba0d5a1f1c5791553dee8dc9ac4c6
马冉旭 <mrx1998@126.com>
2022-12-16 17:31:16 +0800

    增加高精度工具类2

--- bigdecimal_helper/bigdecimal.go
+++ bigdecimal_helper/bigdecimal.go
@@ -4,37 +4,22 @@ import "github.com/shopspring/decimal"
 
 var (
 	//ZeroToTen 0-10
-	ZeroToTen []decimal.Decimal
+	ZeroToTen = []decimal.Decimal{
+		decimal.NewFromInt(0),
+		decimal.NewFromInt(1),
+		decimal.NewFromInt(2),
+		decimal.NewFromInt(3),
+		decimal.NewFromInt(4),
+		decimal.NewFromInt(5),
+		decimal.NewFromInt(6),
+		decimal.NewFromInt(7),
+		decimal.NewFromInt(8),
+		decimal.NewFromInt(9),
+		decimal.NewFromInt(10),
+	}
 
 	Zero = ZeroToTen[0]
 	One  = ZeroToTen[1]
 	Two  = ZeroToTen[2]
 	Ten  = ZeroToTen[10]
 )
-
-func init() {
-	d0 := decimal.NewFromInt(0)
-	d1 := decimal.NewFromInt(1)
-	d2 := decimal.NewFromInt(2)
-	d3 := decimal.NewFromInt(3)
-	d4 := decimal.NewFromInt(4)
-	d5 := decimal.NewFromInt(5)
-	d6 := decimal.NewFromInt(6)
-	d7 := decimal.NewFromInt(7)
-	d8 := decimal.NewFromInt(8)
-	d9 := decimal.NewFromInt(9)
-	d10 := decimal.NewFromInt(10)
-	ZeroToTen = []decimal.Decimal{
-		d0,
-		d1,
-		d2,
-		d3,
-		d4,
-		d5,
-		d6,
-		d7,
-		d8,
-		d9,
-		d10,
-	}
-}
---
+++ bigdecimal_helper/bigdecimal_test.go
@@ -0,0 +1,11 @@
+package bigdecimal_helper
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestName(t *testing.T) {
+	fmt.Println(Zero)
+	fmt.Println(ZeroToTen)
+}
